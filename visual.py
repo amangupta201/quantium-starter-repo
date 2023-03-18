1f963f6e80de7737a83546f57f982a593ecc4633
amangupta201 <aman.thedeveloper08@gmail.com>
2023-03-18 22:41:06 +0530

    Modified Visual File

--- visual.py
+++ visual.py
@@ -1,24 +1,38 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
+
 import pandas as pd
-
-
-app = Dash(__name__)
 
 df = pd.read_csv('modified_data.csv')
 
-fig = px.scatter(df, x="sales", y="date",
-                 log_x=True, size_max=60)
+app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        df['sales'].min(),
+        df['sales'].max(),
+        step=None,
+        value=df['sales'].min(),
+        marks={str(sales): str(sales) for sales in df['sales'].unique()},
+        id='sales-slider'
     )
 ])
+
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    Input('sales-slider', 'value'))
+def update_figure(selected_sales):
+    filtered_df = df[df.sales == selected_sales]
+
+    fig = px.scatter(filtered_df, x="sales", y="date",
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
